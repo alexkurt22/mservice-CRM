b4c9d697e7f80368e0858083abb76631223227b8
alexkurt22 <alchpan1997@gmail.com>
2026-07-14 16:15:19 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,31 +25,4 @@ class MyApp extends StatelessWidget {
       home: const AdminLayout(),
     );
   }
-}import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'admin_layout.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'M-Service CRM',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-        useMaterial3: true,
-      ),
-      home: const AdminLayout(),
-    );
-  }
 }
