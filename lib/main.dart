444ccdd43f91c52ea76d6ee4b4df55bb790b1df4
alexkurt22 <alchpan1997@gmail.com>
2026-07-26 01:22:29 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:shared_preferences/shared_preferences.dart'; // Добавлен локальный хранитель сессии
-import 'package:cloud_firestore/cloud_firestore.dart'; // Добавлен для проверки статуса в базе
+import 'package:shared_preferences/shared_preferences.dart'; 
+import 'package:cloud_firestore/cloud_firestore.dart'; 
 import 'firebase_options.dart';
-import 'screens/dashboard_screen.dart'; // Указываем прямой путь на наш новый Дашборд
-import 'screens/login_screen.dart'; // Импортируем будущий экран входа
+import 'screens/dashboard_screen.dart'; 
+import 'screens/login_screen.dart'; 
+import 'screens/settings_screen.dart'; // Важно за преносот на темата
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,13 +13,12 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // Проверяем перед запуском: есть ли сохраненный номер сотрудника в телефоне
   final prefs = await SharedPreferences.getInstance();
   final savedPhone = prefs.getString('employee_phone');
+  final bool isDarkSaved = prefs.getBool('is_dark_mode') ?? false;
 
   Widget startScreen = const LoginScreen();
 
-  // Если номер есть, на всякий случай проверяем в Firestore, не заблокирован ли он
   if (savedPhone != null && savedPhone.isNotEmpty) {
     try {
       final doc = await FirebaseFirestore.instance.collection('employees').doc(savedPhone).get();
@@ -26,27 +26,62 @@ void main() async {
         startScreen = const DashboardScreen();
       }
     } catch (e) {
-      debugPrint('Ошибка проверки сессии сотрудника: $e');
+      debugPrint('Грешка при проверка на сесијата на вработениот: $e');
     }
   }
 
-  runApp(MyApp(startScreen: startScreen));
+  runApp(MyApp(startScreen: startScreen, initialDarkMode: isDarkSaved));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final Widget startScreen;
-  const MyApp({super.key, required this.startScreen});
+  final bool initialDarkMode;
+
+  const MyApp({super.key, required this.startScreen, required this.initialDarkMode});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late bool _isDarkMode;
+
+  @override
+  void initState() {
+    super.initState();
+    _isDarkMode = widget.initialDarkMode;
+  }
+
+  // Функција за промена и зачувување на темата
+  Future<void> _toggleTheme(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('is_dark_mode', value);
+    setState(() {
+      _isDarkMode = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // Убираем красную ленточку "DEBUG" в углу
+      debugShowCheckedModeBanner: false, 
       title: 'M-Service CRM',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey), // Корпоративный цвет
+        brightness: Brightness.light,
+        primarySwatch: Colors.blueGrey,
+        scaffoldBackgroundColor: Colors.grey[50],
+        cardColor: Colors.white,
         useMaterial3: true,
       ),
-      home: startScreen, // Загружаем стартовый экран в зависимости от авторизации
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blueGrey,
+        scaffoldBackgroundColor: Colors.grey[950],
+        cardColor: Colors.grey[900],
+        useMaterial3: true,
+      ),
+      themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      home: widget.startScreen,
     );
   }
 }
