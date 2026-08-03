5dbdb5e850dd4ed08186a0258f65fb739a396750
alexkurt22 <alchpan1997@gmail.com>
2026-08-03 15:36:39 +0500

    Update dashboard_screen.dart

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -5,7 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import 'users_screen.dart';
 import 'orders_screen.dart';
